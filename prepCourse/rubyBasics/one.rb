c7709889ed1bf145088edb3a089636ac3b9fcec2
Josh Lee <Lee45276@gmail.com>
2015-06-06 20:40:39 +0800

    Completed three

--- prepCourse/rubyBasics/one.rb
+++ prepCourse/rubyBasics/one.rb
@@ -23,7 +23,20 @@ def two
 	
 end
 
-two
+def three
+  movies = {:movie1 => 1975,
+            :movie2 => 2004,
+            :movie3 => 2013,
+            :movie4 => 2001,
+            :movie5 => 1981
+          }
+
+  movies.each { |k, v| puts v}
+end
+
+three
+
+
 
 
 
