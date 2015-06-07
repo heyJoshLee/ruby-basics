aecae7f32abdcf082ca42bcca8baebc2ff2715d4
Josh Lee <Lee45276@gmail.com>
2015-06-07 18:38:29 +0800

    added age method and .times to name method

--- prepCourse/rubyBasics/variables.rb
+++ prepCourse/rubyBasics/variables.rb
@@ -7,6 +7,25 @@ def name
 	puts "What's your name?"
 	name = gets.chomp
 	puts "Hi there #{name}. How are you doing today?"
+
+	puts "Here is your name 10 times:"
+
+	10.times {puts name}
 end
 
-name
+def age
+	puts "Write a program called age.rb that asks a user "
+	puts "how old they are and then tells them how old they "
+	puts "will be in 10, 20, 30 and 40 years. Below is the "
+	puts "output for someone 20 years old."
+
+	puts "How old are you?"
+	age = gets.chomp.to_i
+
+	puts "In 10 years you will be:\n" + (10 + age).to_s
+	puts "In 10 years you will be:\n" + (20 + age).to_s
+	puts "In 10 years you will be:\n" + (30 + age).to_s
+	puts "In 10 years you will be:\n" + (40 + age).to_s
+
+end
+
