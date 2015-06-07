1162550c501586ce288da54760b2222f0ce63c38
Josh Lee <Lee45276@gmail.com>
2015-06-07 19:21:40 +0800

    Completed methods.rb

---
+++ prepCourse/rubyBasics/methods.rb
@@ -0,0 +1,17 @@
+def greeting(name)
+	puts "Write a program that contains a method "
+	puts "called greeting that takes a name as its "
+	puts "parameter. It then prints a greeting message"
+	puts "with the name included in it."
+
+	puts "Hi there #{name}."
+end
+
+def multiply(a, b)
+	puts "Write a program that includes a method called "
+	puts "multiply that takes two arguments and returns the "
+	puts "product of the two numbers."
+
+	return a * b # nothing will show up on the screem unless I put puts before calling the method
+end
+
