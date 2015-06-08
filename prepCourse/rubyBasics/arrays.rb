909afd5586c08ef0caa832232d2c50814f514f48
Josh Lee <Lee45276@gmail.com>
2015-06-08 22:44:24 +0800

    Finished arrays

--- prepCourse/rubyBasics/arrays.rb
+++ prepCourse/rubyBasics/arrays.rb
@@ -15,4 +15,31 @@ def one
 	 end
 end
 
-one
+def seven
+	puts "Write a program that iterates over an array "
+	puts "and builds a new array that is the result of "
+	puts "incrementing each value in the original array "
+	puts "by a value of 2. You should have two arrays at "
+	puts "the end of this program, The original array and "
+	puts "the new array you've created. Print both arrays "
+	puts "to the screen using the p method instead of puts.\n\n"
+
+	arr1 = [1,2,3,4,5]
+	arr2 = []
+
+	arr1.each do |i|
+		arr2.push(i + 2)
+	end
+
+	p arr1
+	p arr2
+
+end
+
+
+
+
+
+
+
+
