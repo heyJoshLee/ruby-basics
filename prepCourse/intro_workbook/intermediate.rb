9f9160c2a17720fc4535eaca28520abf75e40102
Josh Lee <Lee45276@gmail.com>
2015-06-10 11:38:09 +0800

    Completed intermediate.rb quiz 3

--- prepCourse/intro_workbook/intermediate.rb
+++ prepCourse/intro_workbook/intermediate.rb
@@ -58,3 +58,64 @@ def q1nine
   end
 end
 
+def q2one
+  puts "Figure out the total age of just the male members of the family"
+  total_male_age = 0
+  munsters.each do |name, details|
+    total_male_age += details["age"] if details["gender"] == "male"
+  end
+end
+
+def q2three
+  puts "One of the most frequently used real-world string properties is "
+  puts "that of 'string substitution', where we take a hard-coded string "
+  puts "and modify it with various parameters from our program."
+  puts "Given this previously seen family hash, print out the name, age "
+  puts "and gender of each family member like this"
+  puts "(Name) is a (age) year old (male or female).\n\n"
+  puts "ANSWER"
+
+  munsters = { 
+  "Herman" => { "age" => 32, "gender" => "male" }, 
+  "Lily" => { "age" => 30, "gender" => "female" }, 
+  "Grandpa" => { "age" => 402, "gender" => "male" }, 
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+  }
+
+  munsters.each do |k, v|
+    current = munsters[k]
+    puts "#{k} is a #{current['age']} old #{current['gender']}"
+  end
+end
+
+def q2four
+  puts "A common idiom used to solve this conundrum is to use the "
+  puts "String#split and String#join methods to break our string up "
+  puts "and then put it back together again."
+  puts "Use this technique to break up the following string and put "
+  puts "it back together with the words in reverse order:"
+  puts "sentence = 'Humpty Dumpty sat on a wall.'\n\n"
+  puts "ANSWER"
+
+  sentence = "Humpty Dumpty sat on a wall."
+
+  sentence.delete!('.')
+  puts sentence.split.reverse.join(" ") + "."
+end
+
+q2four
+
+
+
+
+
+
+
+
+
+
+
+
+
+
